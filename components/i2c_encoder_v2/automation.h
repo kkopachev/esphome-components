01ab6a214588ae53150273ee42655b1f7c0845cb
Konstantin Kopachev <tenzzor@gmail.com>
2025-12-09 10:03:13 -0800

    Change function parameter types to const references (esphome breacking change)

--- components/i2c_encoder_v2/automation.h
+++ components/i2c_encoder_v2/automation.h
@@ -12,9 +12,9 @@ template<typename... Ts> class I2CEncoderSetPositionAction : public Action<Ts...
   I2CEncoderSetPositionAction(I2CEncoderV2Component *parent) : parent_(parent) {}
 
   void set_position(float position) { this->position_ = position; }
-  void set_position(std::function<float(Ts...)> &&f) { this->position_func_ = f; }
+  void set_position(std::function<float(const Ts&...)> &&f) { this->position_func_ = f; }
 
-  void play(Ts... args) override {
+  void play(const Ts &... args) override {
     float pos;
     if (this->position_func_.has_value()) {
       pos = (*this->position_func_)(args...);
@@ -27,14 +27,14 @@ template<typename... Ts> class I2CEncoderSetPositionAction : public Action<Ts...
  protected:
   I2CEncoderV2Component *parent_;
   float position_{0};
-  optional<std::function<float(Ts...)>> position_func_{};
+  optional<std::function<float(const Ts&...)>> position_func_{};
 };
 
 template<typename... Ts> class I2CEncoderIncrementAction : public Action<Ts...> {
  public:
   I2CEncoderIncrementAction(I2CEncoderV2Component *parent) : parent_(parent) {}
 
-  void play(Ts... args) override { this->parent_->increment(); }
+  void play(const Ts &... args) override { (void)args; this->parent_->increment(); }
 
  protected:
   I2CEncoderV2Component *parent_;
@@ -44,7 +44,7 @@ template<typename... Ts> class I2CEncoderDecrementAction : public Action<Ts...> 
  public:
   I2CEncoderDecrementAction(I2CEncoderV2Component *parent) : parent_(parent) {}
 
-  void play(Ts... args) override { this->parent_->decrement(); }
+  void play(const Ts &... args) override { (void)args; this->parent_->decrement(); }
 
  protected:
   I2CEncoderV2Component *parent_;
